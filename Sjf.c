e8438ca98fdbfb99c9435d01a016164074e57821
kasrarezaee <154625988+kasrarezaee@users.noreply.github.com>
2025-12-16 15:16:16 +0330

    Create Sjf.c

---
+++ Sjf.c
@@ -0,0 +1,69 @@
+#include <stdio.h>
+
+struct Process {
+    int id;
+    int burstTime;
+    int waitingTime;
+    int turnaroundTime;
+};
+
+int main() {
+    int n, i, j;
+    struct Process temp;
+    float avgWait = 0, avgTurnaround = 0;
+    
+    printf("Enter the number of processes: ");
+    scanf("%d", &n);
+    
+    struct Process p[n];
+    
+    printf("\nEnter process details:\n");
+    for(i = 0; i < n; i++) {
+        p[i].id = i + 1;
+        printf("P%d burst time: ", i + 1);
+        scanf("%d", &p[i].burstTime);
+    }
+    
+    // Sort by burst time (ascending) - SJF
+    for(i = 0; i < n - 1; i++) {
+        for(j = 0; j < n - i - 1; j++) {
+            if(p[j].burstTime > p[j+1].burstTime) {
+                temp = p[j];
+                p[j] = p[j+1];
+                p[j+1] = temp;
+            }
+        }
+    }
+    
+    // Calculate waiting and turnaround times
+    p[0].waitingTime = 0;
+    p[0].turnaroundTime = p[0].burstTime;
+    
+    for(i = 1; i < n; i++) {
+        p[i].waitingTime = p[i-1].turnaroundTime;
+        p[i].turnaroundTime = p[i].waitingTime + p[i].burstTime;
+    }
+    
+    // Display results
+    int totalWait = 0;
+    int totalTurnaround = 0;
+    
+    printf("\nSJF Scheduling Results:\n");
+    printf("PID\tBurst\tWait\tTurnaround\n");
+    
+    for(i = 0; i < n; i++) {
+        totalWait += p[i].waitingTime;
+        totalTurnaround += p[i].turnaroundTime;
+        
+        printf("P%d\t%d\t%d\t%d\n", 
+               p[i].id, p[i].burstTime, p[i].waitingTime, p[i].turnaroundTime);
+    }
+    
+    avgWait = (float)totalWait / n;
+    avgTurnaround = (float)totalTurnaround / n;
+    
+    printf("\nAverage waiting time: %.2f\n", avgWait);
+    printf("Average turnaround time: %.2f\n", avgTurnaround);
+    
+    return 0;
+}
